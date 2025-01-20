aa6e8fc20fab43ca9a44a61e933ab8d1d3deb2e4
Cr1st1anPM <josuepmora@gmail.com>
2025-01-20 16:25:08 -0600

    [ADD] New invoice report template.

--- tvcr_changes/models/account_move.py
+++ tvcr_changes/models/account_move.py
@@ -41,6 +41,31 @@ class AccountInvoiceElectronic(models.Model):
                 #     grouped_lines[key].append(line)
         return list(grouped_lines.values())
 
+    @api.depends('amount_total')
+    def group_lines_by_product(self):
+        grouped_lines = defaultdict(list)
+        for line in self.invoice_line_ids:
+            key = line.product_id.id
+            if key not in grouped_lines:
+                grouped_lines[key] = {
+                    'descripcion': line.product_id.name,
+                    'cantidad': 1 ,
+                    'precio_unitario': line.price_unit,
+                    'bruto': line.price_unit,
+                    'descuento': line.price_unit * line.discount / 100,
+                    'neto': line.price_subtotal
+                }
+            else:
+                grouped_lines[key]['cantidad'] += 1
+                grouped_lines[key]['bruto'] += line.price_unit
+                grouped_lines[key]['descuento'] += line.price_unit * line.discount / 100
+                grouped_lines[key]['neto'] += line.price_subtotal
+                # else:
+                #     # Si no tiene orden de venta asociada, agrupamos solo por descripcion
+                #     key = (line.name, None)
+                #     grouped_lines[key].append(line)
+        return list(grouped_lines.values())
+
     def _get_name_invoice_report(self):
         """ This method need to be inherit by the localizations if they want to print a custom invoice report instead of
         the default one. For example please review the l10n_ar module """
