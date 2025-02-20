ed37bebda445ca0aa343f414cde871937d197593
Cr1st1anPM <josuepmora@gmail.com>
2025-02-20 11:31:27 -0600

    ir.ui.menu rule changes

--- tvcr_pagos/models/res_user_role.py
+++ tvcr_pagos/models/res_user_role.py
@@ -4,7 +4,6 @@ from odoo import models, fields, api, exceptions, _
 class HideMenuRole(models.Model):
     _inherit = 'res.users.role'
 
-    admin_role = fields.Boolean('Rol de Administracion')
     menu_ids = fields.Many2many('ir.ui.menu', 'Menus', domain=[('parent_id', '=', False), ('id', 'not in', [1,81])])
 
     def write(self, vals):
@@ -16,25 +15,10 @@ class HideMenuRole(models.Model):
                 })
         return res
 
-    def _assign_admin_role(self):
-        self.ensure_one()
-        menu_ids = self.env['ir.ui.menu'].search([('parent_id', '=', False), ('id', 'not in', [1,81]), ('active', '=', True)])
-        for m in menu_ids:
-             m.write({
-                'role_ids': [(4, self.id)],
-            })
-        self.write({
-            'menu_ids': [(6, 0, menu_ids.ids)]
-        })
-        self.admin_role = True
-
     def _reset_menu_roles(self):
         menus = self.env['ir.ui.menu'].search([('parent_id', '=', False),('role_ids', '!=', False)])
         roles = self.search([])
         for m in menus:
-            m.role_ids = False
+            m.sudo().write({'role_ids': False})
         for r in roles:
-            r.admin_role = False
-            r.menu_ids = False
-
-
+            r.sudo().write({'admin_role': False, 'menu_ids': False})
